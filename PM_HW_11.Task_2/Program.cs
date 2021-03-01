957b97b1892747f208b1cfc56f5e6d768912250c
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-03-01 23:44:04 +0200

    Initial commit

---
+++ PM_HW_11.Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PM_HW_11.Task_2
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
